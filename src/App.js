7610b7f76058e86e833fa66ef16aa94f12ebd09c
KimThang <18521388@gm.uit.edu.vn>
2021-05-05 12:04:03 +0700

    end commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react'
 import Dropdown from './components/Dropdown';
 import Hero from './components/Hero';
+import InfoSection from './components/InfoSection';
 import Navbar from './components/Navbar'
 import { sliderData } from './data/SliderData';
+import { infoData } from './data/InfoData';
 import GlobalStyle from './globalStyle';
 
 function App() {
@@ -17,6 +19,7 @@ function App() {
       <Navbar toggle={toggle}/>
       <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Hero slides={sliderData}></Hero>
+      <InfoSection {...infoData}/>
     </>
   );
 }
---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/hourse1.jpg'
+import ImageTwo from '../images/hourse2.jpg'
+
+export const infoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    paragrapTow: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    image: ImageOne,
+    reverse: false,
+    delay: 100,
+    buttonLabel: 'Views Home'
+};
+
+export const infoDataTwo = {
+    heading: 'Moders Designs',
+    paragraphOne: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    paragrapTow: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300,
+    buttonLabel: 'Views Home'
+}
